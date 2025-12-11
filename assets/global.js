03f3c4bb90b811808b9fe7764fc0c6af4384833c
Ben Hankins <WBHankins93@gmail.com>
2025-12-10 20:07:17 -0600

    Initial Shopify Liquid theme structure - clean slate conversion from Next.js

---
+++ assets/global.js
@@ -0,0 +1,5 @@
+// Global JavaScript for Big Butt Association Shopify Theme
+// This will be expanded with cart functionality and other features
+
+console.log('Big Butt Association theme loaded');
+
